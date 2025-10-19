d5698384f787924db59d534a04d3a3fefdcfe092
Tony Chen <yang.chen2@anu.edu.au>
2025-10-19 18:21:29 +1100

    Update License

--- lib/src/services/solid_security_key_notifier.dart
+++ lib/src/services/solid_security_key_notifier.dart
@@ -2,6 +2,10 @@
 ///
 /// Copyright (C) 2025, Software Innovation Institute, ANU.
 ///
+/// Licensed under the MIT License (the "License").
+///
+/// License: https://choosealicense.com/licenses/mit/.
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
@@ -9,8 +13,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
