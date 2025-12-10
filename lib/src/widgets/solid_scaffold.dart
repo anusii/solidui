8a2e815d57498efd2cf833d9d4355ab907e18d48
Williams <lambadajester50@gmail.com>
2025-12-10 13:32:27 +1100

    new

--- lib/src/widgets/solid_scaffold.dart
+++ lib/src/widgets/solid_scaffold.dart
@@ -509,6 +509,7 @@ class SolidScaffoldState extends State<SolidScaffold> {
             ),
             _onMenuSelected,
             widget.onShowAlert,
+            widget.menu,  // Pass menu items for IndexedStack
           );
     return NotificationListener<SecurityKeyStatusChangedNotification>(
       onNotification: (notification) {
