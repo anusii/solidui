1d8bb14ed61d5c503276a10a4be28d58074d4551
Williams <lambadajester50@gmail.com>
2026-01-07 02:33:53 +1100

    new

--- lib/solidui.dart
+++ lib/solidui.dart
@@ -95,6 +95,7 @@ export 'src/utils/solid_file_operations.dart';
 export 'src/utils/solid_alert.dart';
 export 'src/utils/solid_pod_helpers.dart'
     show loginIfRequired, getKeyFromUserIfRequired;
+export 'src/utils/solid_notifications.dart';
 
 export 'src/widgets/solid_format_info_card.dart';
 
