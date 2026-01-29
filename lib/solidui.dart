ba95c41a808a196413b8bf4197c5476ac2bdffd2
Graham Williams <graham.williams@togaware.com>
2026-01-30 10:51:43 +1100

    Export SecurityKeyStatusChangedNotification

--- lib/solidui.dart
+++ lib/solidui.dart
@@ -94,6 +94,7 @@ export 'src/utils/file_operations.dart';
 export 'src/utils/is_text_file.dart';
 export 'src/utils/solid_file_operations.dart';
 export 'src/utils/solid_alert.dart';
+export 'src/utils/solid_notifications.dart';
 export 'src/utils/solid_pod_helpers.dart'
     show loginIfRequired, getKeyFromUserIfRequired;
 
