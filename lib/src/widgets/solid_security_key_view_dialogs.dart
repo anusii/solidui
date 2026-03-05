3c7b4159fa9851a174b4fa3ef23a00c9fd3bf092
Tony Chen <yang.chen2@anu.edu.au>
2026-03-06 07:25:44 +1100

    Add explanatory text to Security Key Management popup

--- lib/src/widgets/solid_security_key_view_dialogs.dart
+++ lib/src/widgets/solid_security_key_view_dialogs.dart
@@ -197,7 +197,24 @@ class SecurityKeyViewDialogs {
           content: ConstrainedBox(
             constraints: const BoxConstraints(maxWidth: 800),
             child: SingleChildScrollView(
-              child: DataTable(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'The security key which is used to encrypt your data '
+                    'within your pod is stored in your pod using a one-way '
+                    'hash, which means we can not show you the actual key, '
+                    'but the information here can be used to verify the key '
+                    'that you supply.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: dialogTheme.colorScheme.onSurfaceVariant,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Center(
+                    child: DataTable(
                 columnSpacing: 30.0,
                 columns: [
                   DataColumn(
@@ -222,6 +239,9 @@ class SecurityKeyViewDialogs {
                   ),
                 ],
                 rows: dataRows,
+              ),
+                  ),
+                ],
               ),
             ),
           ),
