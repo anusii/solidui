10502c3024cb71fd83f0c8d371b301a8569bed57
cdawei <>
2026-09-02 14:02:30 +1000

    Use material_ui package

--- lib/src/widgets/secret_text_field.dart
+++ lib/src/widgets/secret_text_field.dart
@@ -28,10 +28,9 @@
 
 library;
 
-import 'package:flutter/material.dart';
-
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:material_ui/material_ui.dart';
 
 import 'package:solidui/src/constants/ui.dart';
 
